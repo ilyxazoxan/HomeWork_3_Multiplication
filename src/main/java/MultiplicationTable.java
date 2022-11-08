1ea2cb6939f704ffb6b5df4892cbb56927a7d293
Ilya Danilov <esozap32@mail.ru>
2022-11-08 23:57:43 +0400

    MultiplicationTable

--- src/main/java/MultiplicationTable.java
+++ src/main/java/MultiplicationTable.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class MultiplicationTable {
+public class MultiplicationTable {
+    public static void main(String[] args) {
+
+        System.out.println("    2  3  4  5  6  7  8  9");
+        int i = 2;
+        while (i < 10) {
+            System.out.print(i + " | ");
+            int j = 2;
+            while (j < 10) {
+                int mul = i * j;
+                if (mul < 10)
+                    System.out.print(mul + "  ");
+                else
+                    System.out.print(mul + " ");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+
 }
